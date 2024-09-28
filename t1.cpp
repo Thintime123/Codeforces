fee5db1117afb033149cd8cdde379632c8dc6431
unknown <2108212640@qq.com>
2024-09-28 10:56:48 +0800

    t1.cpp

---
+++ t1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define int ll
+#define pii pair<int, int>
+#define all(x) x.begin(), x.end()
+
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 2;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    vector<int> arr(3);
+    for (int c : arr)
+        cin >> c;
+    cout << arr[0];
+
+    return 0;
+}
