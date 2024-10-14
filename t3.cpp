77faf810832c886f7d62d4631f26042b9f7eea09
unknown <2108212640@qq.com>
2024-10-14 23:44:28 +0800

    t3.cpp

--- t3.cpp
+++ t3.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 const int MOD = 1000000007;
-const int MAX_N = 100000;
+const int MAX_N = 100;
 
 // 预处理数组，存储组合数
-vector<vector<int>> C;
+vector<vector<int>> C(MAX_N + 1, vector<int>(MAX_N + 1));
 
-void preprocess(int max_n) {
-    // 动态创建二维数组的行
-    C.resize(max_n + 1);
-    
+void preprocess() {
     // 初始化组合数数组
-    for (int n = 0; n <= max_n; ++n) {
-        C[n].resize(n + 1); // 动态创建每一行的列
+    for (int n = 0; n <= MAX_N; ++n) {
         C[n][0] = 1;  // C[n][0] = 1
         C[n][n] = 1;  // C[n][n] = 1
         for (int k = 1; k < n; ++k) {
@@ -30,17 +25,15 @@ int main() {
     vector<int> n_values(t), k_values(t);
 
     // 输入 n 和 k 的值
-    int max_n = 0;
     for (int i = 0; i < t; ++i) {
         cin >> n_values[i];
-        max_n = max(max_n, n_values[i]); // 记录最大的n
     }
     for (int i = 0; i < t; ++i) {
         cin >> k_values[i];
     }
 
-    // 预处理直到最大的 n 值
-    preprocess(max_n);
+    // 预处理所有的 C[n][k]
+    preprocess();
 
     // 对每个查询，输出对应的 C[n][k]
     for (int i = 0; i < t; ++i) {
