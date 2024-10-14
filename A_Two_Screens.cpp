5c6ed4c3ac5101cbc2a7657619320dc69a3cfafa
unknown <2108212640@qq.com>
2024-10-14 22:45:12 +0800

    A_Two_Screens.cpp

---
+++ A_Two_Screens.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define int ll
+#define pii pair<int, int>
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define fer(i, n) for (int i = 0; i < n; ++i)
+#define ferr(i, n) for (int i = 1; i <= n; ++i)
+
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 2;
+
+void solve()
+{
+    string s, t;
+    cin >> s >> t;
+    int d = 0;
+    for (int i = 0; i < s.size() && i < t.size(); i++)
+    {
+        if (s[i] != t[i])
+        {
+            d = i;
+            break;
+        }
+    }
+    
+    int ans = d + 1 + s.size() - d + t.size() - d;
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    int T = 1;
+    cin >> T;
+
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
