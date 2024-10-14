de61062ded024e794454db5fd5a3a74c16ab770e
unknown <2108212640@qq.com>
2024-10-15 00:02:25 +0800

    t3.cpp

--- t3.cpp
+++ t3.cpp
@@ -3,32 +3,38 @@
 using namespace std;
 
 const int MOD = 1000000007;
-const int MAX_N = 100;
+const int MAX_N = 133;
 
 // 预处理数组，存储组合数
 vector<vector<int>> C(MAX_N + 1, vector<int>(MAX_N + 1));
 
-void preprocess() {
+void preprocess()
+{
     // 初始化组合数数组
-    for (int n = 0; n <= MAX_N; ++n) {
-        C[n][0] = 1;  // C[n][0] = 1
-        C[n][n] = 1;  // C[n][n] = 1
-        for (int k = 1; k < n; ++k) {
-            C[n][k] = (C[n][k - 1] + C[n - 1][k - 1]) % MOD;  // 错误公式
+    for (int n = 0; n <= MAX_N; ++n)
+    {
+        C[n][0] = 1; // C[n][0] = 1
+        C[n][n] = 1; // C[n][n] = 1
+        for (int k = 1; k < n; ++k)
+        {
+            C[n][k] = (C[n][k - 1] + C[n - 1][k - 1]) % MOD; // 错误公式
         }
     }
 }
 
-int main() {
+int main()
+{
     int t;
-    cin >> t;  // 输入查询对的数量
+    cin >> t; // 输入查询对的数量
     vector<int> n_values(t), k_values(t);
 
     // 输入 n 和 k 的值
-    for (int i = 0; i < t; ++i) {
+    for (int i = 0; i < t; ++i)
+    {
         cin >> n_values[i];
     }
-    for (int i = 0; i < t; ++i) {
+    for (int i = 0; i < t; ++i)
+    {
         cin >> k_values[i];
     }
 
@@ -36,7 +42,8 @@ int main() {
     preprocess();
 
     // 对每个查询，输出对应的 C[n][k]
-    for (int i = 0; i < t; ++i) {
+    for (int i = 0; i < t; ++i)
+    {
         int n = n_values[i];
         int k = k_values[i];
         cout << C[n][k] << endl;
