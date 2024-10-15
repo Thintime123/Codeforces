4d0673b635d487d427101dd305a8428e052fd83c
unknown <2108212640@qq.com>
2024-10-15 13:10:18 +0800

    C_New_Game.cpp

---
+++ C_New_Game.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define int ll
+#define pii pair<int, int>
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define fer(i, n) for (int i = 0; i < n; ++i)
+#define ferr(i, n) for (int i = 1; i <= n; ++i)
+
+const int MOD = 1e9 + 7;
+const int N = 1e5 + 2;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(N);
+    fer(i, n) cin >> arr[i];
+
+    sort(all(arr));
+    fer(i, n) cout << arr[i] << ' ';
+    cout << endl;
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    int T = 1;
+    cin >> T;
+
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
