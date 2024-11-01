d1f6480c3ce45ab275f01c97db2e190a54c78edd
unknown <2108212640@qq.com>
2024-11-01 17:08:12 +0800

    t1.cpp

--- t1.cpp
+++ t1.cpp
@@ -1,23 +1,30 @@
-#include <iostream>
-#include <chrono>
+#include<bits/stdc++.h>
+#include<chrono>
+using namespace std;
 
-// 假设要测试的函数
-int someFunction() {
-    int sum = 0;
-    for (int i = 0; i < 10000; i++) {
-        sum += i;
-    }
-    return sum;
-}
+#define ll long long
+//#define int ll
+#define pii pair<int, int>
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define fer(i, m, n) for(int i = m; i < n; ++i)
+#define ferd(i, m, n) for(int i = m; i >= n; --i)
 
-int main() {
-    auto start = std::chrono::high_resolution_clock::now();
-    int result = someFunction();
-    auto end = std::chrono::high_resolution_clock::now();
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 2;
 
-    std::chrono::duration<double, std::milli> elapsed = end - start;
-    std::cout << "Function took " << elapsed.count() << " milliseconds." << std::endl;
-    std::cout << "Result: " << result << std::endl;
 
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    auto start_time = clock();
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+
+#ifdef LOCAL
+    cout << "\nTime : ";
+    cout << (double)(clock() - start_time) / CLOCKS_PER_SEC * 1000 << "ms\n";
+#endif
     return 0;
 }
