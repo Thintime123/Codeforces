990ee6a9062291f1e2efc4dec9a4a6702343868e
unknown <2108212640@qq.com>
2024-11-01 16:46:33 +0800

    t2.cpp

--- t2.cpp
+++ t2.cpp
@@ -21,7 +21,7 @@ signed main() {
 
     string s;
     while(getline(cin, s))
-        cout << "\"" << s << "\\n\"," << endl; 
+        cout << "\"" << s << "\"," << endl; 
 
 #ifdef LOCAL
     cout << "\nTime : ";
