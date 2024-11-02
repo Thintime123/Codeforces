febac56277d9f45bd2912adb1cbf62c7eef662fc
unknown <2108212640@qq.com>
2024-11-02 19:57:43 +0800

    t1.cpp sample correct

--- t1.cpp
+++ t1.cpp
@@ -1,33 +1,31 @@
-#include <bits/stdc++.h>
-#include <chrono>
+#include<bits/stdc++.h>
+
 using namespace std;
 
 #define ll long long
-// #define int ll
+//#define int ll
 #define pii pair<int, int>
-#define all(x) x.begin(), x.end()
+#define all(x) x.begin(),x.end()
 #define endl '\n'
-#define fer(i, m, n) for (int i = m; i < n; ++i)
-#define ferd(i, m, n) for (int i = m; i >= n; --i)
+#define fer(i, m, n) for(int i = m; i < n; ++i)
+#define ferd(i, m, n) for(int i = m; i >= n; --i)
 
 const int MOD = 1e9 + 7;
 const int N = 2e5 + 2;
 
-signed main()
-{
+
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    auto start_time = clock();
 
-    auto t1 = std::chrono::high_resolution_clock::now();
-    int a = 1;
-    fer(i, 1, 10000000) a++, a--;
-    auto t2 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> t = t2 - t1;
-    cout << t.count() << "ms\n";
-#ifdef LOCAL
-    cout << "\nTime : ";
-    cout << (double)(clock() - start_time) / CLOCKS_PER_SEC * 1000 << "ms\n";
-#endif
+    int n;
+    scanf("%d", &n);
+    ll s = n * (n - 1) / 2;
+    fer(i, 1, n){
+        int a;
+        scanf("%d", &a);
+        s -= a;
+    }
+    printf("%lld", s);
     return 0;
 }
