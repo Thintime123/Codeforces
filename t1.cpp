9f31cf681db905d86af2bce2d74f86e63a7ed8c1
unknown <2108212640@qq.com>
2024-11-02 20:11:37 +0800

    t1.cpp sample correct

--- t1.cpp
+++ t1.cpp
@@ -1,31 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 #define ll long long
-//#define int ll
+// #define int ll
 #define pii pair<int, int>
-#define all(x) x.begin(),x.end()
+#define all(x) x.begin(), x.end()
 #define endl '\n'
-#define fer(i, m, n) for(int i = m; i < n; ++i)
-#define ferd(i, m, n) for(int i = m; i >= n; --i)
+#define fer(i, m, n) for (int i = m; i < n; ++i)
+#define ferd(i, m, n) for (int i = m; i >= n; --i)
 
 const int MOD = 1e9 + 7;
 const int N = 2e5 + 2;
 
-
-signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n;
-    scanf("%d", &n);
-    ll s = n * (n + 1) / 2;
-    fer(i, 1, n){
-        int a;
-        scanf("%d", &a);
-        s -= a;
+signed main()
+{
+    ll n;
+    scanf("%lld", &n);
+    ll s = 0;
+    fer(i, 1, n)
+    {
+        ll a;
+        scanf("%lld", &a);
+        s += i - a;
     }
+    s += n;
     printf("%lld", s);
     return 0;
 }
