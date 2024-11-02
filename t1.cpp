682f6bac706bfc1bea9cfb1ff88267d2be929b74
unknown <2108212640@qq.com>
2024-11-02 21:08:16 +0800

    t1.cpp sample correct

--- t1.cpp
+++ t1.cpp
@@ -1,36 +1,64 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 #define ll long long
-// #define int ll
+//#define int ll
 #define pii pair<int, int>
-#define all(x) x.begin(), x.end()
+#define all(x) x.begin(),x.end()
 #define endl '\n'
-#define fer(i, m, n) for (int i = m; i < n; ++i)
-#define ferd(i, m, n) for (int i = m; i >= n; --i)
+#define fer(i, m, n) for(int i = m; i < n; ++i)
+#define ferd(i, m, n) for(int i = m; i >= n; --i)
+
+template <typename T>
+inline T read()
+{
+    T x = 0;
+    int y = 1;
+    char ch = getchar();
+    while (ch > '9' || ch < '0')
+    {
+        if (ch == '-')
+            y = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * y;
+}
+
+template <typename T>
+inline void write(T x)
+{
+    if (x < 0)
+    {
+        putchar('-');
+        x = -x;
+    }
+    if (x >= 10)
+    {
+        write(x / 10);
+    }
+    putchar(x % 10 + '0');
+}
 
 const int MOD = 1e9 + 7;
 const int N = 2e5 + 2;
 
-signed main()
-{
-    int n;
-    scanf("%d", &n);
-    n++;
-    bool *vis = new bool[n / 2 + 2]{};
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n = read<ll>();
+    ll s = n * (n + 1) / 2;
     fer(i, 1, n){
-        int a;
-        scanf("%d", &a);
-        if(vis[a % (n / 2)] == false) vis[a % (n / 2)] = true;
-        else vis[a % (n / 2)] = false;
+        int a = read<int>();
+        s -= a;
     }
-    fer(i, 0, n / 2 + 2){
-        if(vis[i]){
-            printf("%d", i + 1);
-            break;
-        }
-    }
-    delete []vis;
+    printf("%lld", s);
     return 0;
 }
