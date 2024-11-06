87c68b61cf22597c19db08eb163d55b762d8d246
unknown <2108212640@qq.com>
2024-11-06 21:31:57 +0800

    t1.cpp sample correct

--- t1.cpp
+++ t1.cpp
@@ -2,13 +2,6 @@
 
 using namespace std;
 
-#define ll long long
-//#define int ll
-#define pii pair<int, int>
-#define all(x) x.begin(),x.end()
-#define endl '\n'
-#define fer(i, m, n) for(int i = m; i < n; ++i)
-#define ferd(i, m, n) for(int i = m; i >= n; --i)
 
 const int MOD = 1e9 + 7;
 const int N = 2e5 + 2;
