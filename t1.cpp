69ee8f5c080abad438a381f44c8a27d2186205fa
unknown <2108212640@qq.com>
2024-11-06 22:38:37 +0800

    t1.cpp sample correct

--- t1.cpp
+++ t1.cpp
@@ -1,17 +1,25 @@
-#include<iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
+#define ll long long
+//#define int ll
+#define pii pair<int, int>
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define fer(i, m, n) for(int i = m; i < n; ++i)
+#define ferd(i, m, n) for(int i = m; i >= n; --i)
 
 const int MOD = 1e9 + 7;
 const int N = 2e5 + 2;
 
 
-int main() {
+signed main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    cout << 2;
+    int a;
+    cin >> a;
+    cout << 111;
+    
 
     return 0;
 }
