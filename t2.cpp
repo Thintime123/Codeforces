d07624f44d758603922b5f87b8fb24eaf0a6fe81
unknown <2108212640@qq.com>
2024-11-08 20:12:12 +0800

    t2.cpp

--- t2.cpp
+++ t2.cpp
@@ -2,30 +2,11 @@
 
 using namespace std;
 
-#define ll long long
-//#define int ll
-#define pii pair<int, int>
-#define all(x) x.begin(),x.end()
-#define endl '\n'
-#define fer(i, m, n) for(int i = m; i < n; ++i)
-#define ferd(i, m, n) for(int i = m; i >= n; --i)
-
-const int MOD = 1e9 + 7;
-const int N = 2e5 + 2;
-
-
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    auto start_time = clock();
 
-    string s;
-    while(getline(cin, s))
-        cout << "\"" << s << "\"," << endl; 
+    cout << 11 << endl;
 
-#ifdef LOCAL
-    cout << "\nTime : ";
-    cout << (double)(clock() - start_time) / CLOCKS_PER_SEC * 1000 << "ms\n";
-#endif
     return 0;
 }
