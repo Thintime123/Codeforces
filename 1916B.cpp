d161b0bab9db3dce748120009eca5d1e9e6de7bf
unknown <2108212640@qq.com>
2024-11-09 15:10:32 +0800

    1916B.cpp

---
+++ 1916B.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+//#define int ll
+#define pii pair<int, int>
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define fer(i, m, n) for(int i = m; i < n; ++i)
+#define ferd(i, m, n) for(int i = m; i >= n; --i)
+
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 2;
+
+int gcd(int a, int b){
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    int x;
+    if(a == 1) x = b * b;
+    else if(b == 1) x = a * a;
+    else{
+        x = a / gcd(a, b) * b;
+    }
+    cout << x << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    auto start_time = clock();
+
+    int T = 1;
+    cin >> T;
+
+    while(T--) {
+        solve();
+    }
+
+#ifdef LOCAL
+    cout << "\nTime : ";
+    cout << (double)(clock() - start_time) / CLOCKS_PER_SEC * 1000 << "ms\n";
+#endif
+    return 0;
+}
