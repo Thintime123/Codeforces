205812b9ffbe42cc08df80428da863f341a360d2
unknown <2108212640@qq.com>
2024-11-10 18:25:02 +0800

    1930B.cpp

--- 1930B.cpp
+++ 1930B.cpp
@@ -20,7 +20,7 @@ void solve() {
     int l = 1, r = n;
     fer(i, 1, n + 1){
         if(i & 1) cout << l++ << ' ';
-        else cout << r++ << ' ';
+        else cout << r-- << ' ';
     }
     cout << '\n';
 }
